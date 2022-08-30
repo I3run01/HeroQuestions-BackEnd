f7fc490d6a24023ef8a5960b6d1f726b18c2d6ba
Bruno Alves Vieira <94388068+I3run01@users.noreply.github.com>
2022-08-30 11:40:49 -0300

    c

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import express, { Request, Response, ErrorRequestHandler } from 'express';
+import path from 'path';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+
+const server = express();
+
+server.use(cors());
+
+server.use(express.static(path.join(__dirname, '../public')));
+server.use(express.urlencoded({ extended: true }));
+
+server.get('/ping', (req: Request, res: Response) => res.json({ pong: true }));
+
+server.use((req: Request, res: Response) => {
+    res.status(404);
+    res.json({ error: 'Endpoint não encontrado.' });
+});
+
+server.listen(process.env.PORT)
