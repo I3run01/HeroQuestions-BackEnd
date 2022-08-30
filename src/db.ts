ab4b961089d7b7bc8624a26e43ed694532ceaf01
Bruno Alves Vieira <94388068+I3run01@users.noreply.github.com>
2022-08-30 09:17:06 -0300

    c

--- src/db.ts
+++
@@ -1,6 +0,0 @@
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { config } from './config'
-
-export const app = initializeApp(config);
-export const analytics = getAnalytics(app);
