1af1e6d9d624ac0722359e3c1ca0676c747c4aea
Bruno Alves Vieira <94388068+I3run01@users.noreply.github.com>
2022-09-01 20:16:35 -0300

    c

--- src/server.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var dotenv_1 = require("dotenv");
-var app_1 = require("./app");
-dotenv_1["default"].config();
-app_1["default"].listen(process.env.PORT || 5000);
