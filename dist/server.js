ea68ad039c9336cb08ce30ad2cb7406b240b5247
Bruno Alves Vieira <94388068+I3run01@users.noreply.github.com>
2022-09-02 19:38:57 -0300

    c

--- dist/server.js
+++ dist/server.js
@@ -5,7 +5,5 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = __importDefault(require("dotenv"));
 const app_1 = __importDefault(require("./app"));
-const http_1 = __importDefault(require("http"));
 dotenv_1.default.config();
-const server = http_1.default.createServer(app_1.default);
-server.listen(process.env.PORT || 5000);
+app_1.default.listen(process.env.PORT || 5000);
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,6 @@
 import dotenv from 'dotenv'
 import app from './app'
-import http from 'http'
 
 dotenv.config()
 
-const server = http.createServer(app)
-
-server.listen(process.env.PORT || 5000)
+app.listen(process.env.PORT || 5000)
