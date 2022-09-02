7c776010a414c649a721e32ae35bcaa1481e589b
Bruno Alves Vieira <94388068+I3run01@users.noreply.github.com>
2022-09-02 19:55:23 -0300

    c

--- dist/server.js
+++ dist/server.js
@@ -5,7 +5,5 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = __importDefault(require("dotenv"));
 const app_1 = __importDefault(require("./app"));
-const heroku_ssl_redirect_1 = __importDefault(require("heroku-ssl-redirect"));
 dotenv_1.default.config();
-app_1.default.use((0, heroku_ssl_redirect_1.default)());
 app_1.default.listen(process.env.PORT || 5000);
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,5 @@
 import dotenv from 'dotenv'
 import app from './app'
-import sslRedirect from 'heroku-ssl-redirect'
-
 dotenv.config()
-app.use(sslRedirect())
 
 app.listen(process.env.PORT || 5000)
