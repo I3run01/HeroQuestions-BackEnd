15410aa290a1a28e841312f130e16b3aeb86695e
Bruno Alves Vieira <94388068+I3run01@users.noreply.github.com>
2022-09-02 19:09:08 -0300

    c

--- dist/database/mongoDB.js
+++ dist/database/mongoDB.js
@@ -14,7 +14,7 @@ const mongoose_1 = require("mongoose");
 const mongoConnect = () => __awaiter(void 0, void 0, void 0, function* () {
     try {
         console.log('Connecting in MongoDB');
-        yield (0, mongoose_1.connect)(process.env.MONGO_URL);
+        yield (0, mongoose_1.connect)('mongodb+srv://Bruno:1515@teppa-database.m10wqfj.mongodb.net/TeppaDB?retryWrites=true&w=majority');
         console.log('MongoDB is successfully connected');
     }
     catch (error) {
