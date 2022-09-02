42c5651faf1cf023d01db907981225351c90bba4
Bruno Alves Vieira <94388068+I3run01@users.noreply.github.com>
2022-09-02 19:10:23 -0300

    c

--- dist/controller/apiController.js
+++ dist/controller/apiController.js
@@ -39,14 +39,12 @@ const ping = (req, res) => {
 };
 exports.ping = ping;
 const register = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    /*
-    if(req.body.email && req.body.password) {
-        let {email, password} = req.body
-
-        const response = await services.createUser(email , password)
-        return res.json(response)
-    }res.json({response: 'E-mail or password not sent', status: false})
-    */
+    if (req.body.email && req.body.password) {
+        let { email, password } = req.body;
+        const response = yield services.createUser(email, password);
+        return res.json(response);
+    }
+    res.json({ response: 'E-mail or password not sent', status: false });
     res.json({ status: true });
 });
 exports.register = register;
