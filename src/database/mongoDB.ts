670fe3349bff79f9346a56d8fb5eb6620b8ab294
Bruno Alves Vieira <94388068+I3run01@users.noreply.github.com>
2022-09-02 18:59:06 -0300

    c

--- src/database/mongoDB.ts
+++ src/database/mongoDB.ts
@@ -3,7 +3,7 @@ import { connect } from "mongoose";
 export const mongoConnect = async () => {
     try {
         console.log('Connecting in MongoDB')
-        await connect(process.env.MONGO_URL as string)
+        await connect('mongodb+srv://Bruno:1515@teppa-database.m10wqfj.mongodb.net/TeppaDB?retryWrites=true&w=majority')
         console.log('MongoDB is successfully connected')
     } catch(error) {
         console.log('mongoDB connection error:', error)
