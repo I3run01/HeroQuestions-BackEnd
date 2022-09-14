884c7c2a32cb5024855646d0c48da92eddde15b2
Bruno Alves Vieira <94388068+I3run01@users.noreply.github.com>
2022-09-14 20:31:27 -0300

    c

--- src/router/api.ts
+++ src/router/api.ts
@@ -8,6 +8,5 @@ router.post('/register', apiController.register)
 router.post('/login', apiController.login)
 router.post('/tokenValidation', apiController.tokenValidation)
 router.post('/heroQuestions', apiController.heroQuestions)
-router.post('/testping', apiController.ping)
 
 export default router
